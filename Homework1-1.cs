803e0e7ac6c131c5c95576325c6e0732523861d5
95DNVR <95denver@gmail.com>
2023-06-03 00:27:35 +0300

    Create Homework1-1.cs

---
+++ Homework1-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int moneyInMyPoсket;
+            int priceOfMoney;
+            uint howManyYearsDoIHaveToRepayTheLoan;
+            float issuePrice;
+            double priceOfRice;
+            char sectorPriseOnTheDrumLetter;
+            string writeToMeSomething;
+            string butWriteSomethingCool;
+            bool areYouHaveEnoughMoney;
+            bool areYouOldEnough;
+
+                
+               
+
+        }
+    }
+}
