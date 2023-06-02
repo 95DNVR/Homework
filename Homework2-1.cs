6efcae1162536b6a2a74b10056cf456daba1982d
95DNVR <95denver@gmail.com>
2023-06-03 00:45:19 +0300

    Create Homework2-1.cs

---
+++ Homework2-1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = (" Ответ будет ноль, так как выражение (31-5*10)/38 в своем ответе содержит дробное число -0,55... однако переменные в нашем коде заданны целочисленные соответственно ничего кроме нуля в консоль не выведится. " +
+                "Фиксится сменой переменной либо при обьявлении (записать не int a float), либо конвертацией значений в самом уравнении при помощи (convert.ToSingle) ");
+        }
+    }
+}
