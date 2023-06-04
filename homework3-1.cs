9c697480b57f5cc66a8855343218580740b0cddc
95DNVR <95denver@gmail.com>
2023-06-04 23:19:55 +0300

    Update homework3-1.cs

--- homework3-1.cs
+++ homework3-1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework3_1
+namespace Cons
 {
     internal class Program
     {
@@ -12,44 +12,36 @@ namespace Homework3_1
         {
             string name;
             string jobTitle;
-            string veryImportantInformation;
+            string importantInformation;
             int age;
             int ageOfRetirement = 65;
             int yearsBeforeRetairement;
-            bool ageCheck;
+            bool isYoungEnought;
             int yearsAfterRetairement;
-
             Console.Write("Здравствуйте, вас приветствует система индетификации завод УралМаш, пожалуйста укажите ваше имя ");
-            name = Console.ReadLine ();
+            name = Console.ReadLine();
             Console.Write(" Пожалуйста, назовите вашу должность ");
-            jobTitle = Console.ReadLine ();
+            jobTitle = Console.ReadLine();
             Console.Write(" Пожалуйста, укажите ваш возраст ");
-            age = Convert.ToInt32(Console.ReadLine ());
-
-            ageCheck = age <= 65; // не додумался как обойтись без маг.числа
-            yearsAfterRetairement = Convert.ToInt32(ageCheck);
-
+            age = Convert.ToInt32(Console.ReadLine());
+            isYoungEnought = age <= ageOfRetirement;
+            yearsAfterRetairement = Convert.ToInt32(isYoungEnought);
             yearsBeforeRetairement = (ageOfRetirement - age) * yearsAfterRetairement;
-            
             Console.WriteLine(" Организация благодарит за предоставленные вами данные. " +
                 "Пожалуйста проверте точность данных " +
                 "Ваше имя " + name + ".Вам " + age + "лет." +
-                "Вы работаете на должности " + jobTitle + 
+                "Вы работаете на должности " + jobTitle +
                 ".До пенсии вам осталось "
-                + yearsBeforeRetairement+ " лет...Задумайтесь)");
+                + yearsBeforeRetairement + " лет...Задумайтесь)");
             Console.Write("Дам... тут возмущаются, что я пошел не по заданию и вместо того что бы спросить очень сложный и важный вопрос про задиак " +
                 "сделал фигню с вычислением пенсионного возраста... так что если вам не трудно напишите пожалуйста ваш знак задиака ");
-            veryImportantInformation = Console.ReadLine ();
+            importantInformation = Console.ReadLine();
             Console.WriteLine(" Организация благодарит за предоставленные вами данные. " +
                "Пожалуйста проверте точность данных " +
                "Ваше имя " + name + ".Вам " + age + " лет." +
                ".Вы работаете на должности " + jobTitle +
                ".До пенсии вам осталось "
-               + yearsBeforeRetairement + " лет...Задумайтесь). Ого вы " + veryImportantInformation + " по знаку задиака. Чел Хароош)");
-
-
-
-
+               + yearsBeforeRetairement + " лет...Задумайтесь). Ого вы " + importantInformation + " по знаку задиака. Чел Хароош)");
         }
     }
 }
