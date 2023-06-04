11ed9e4da32f036ab30ec6c0a3bc1390974e33be
95DNVR <95denver@gmail.com>
2023-06-05 01:45:39 +0300

    Update Homework4-1.cs

--- Homework4-1.cs
+++ Homework4-1.cs
@@ -4,24 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework4_1
+namespace hgjhgcj
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int numberOfPicture = 52;
+            int numberOfPictures = 52;
             int numberOfPicturesInOneRow = 3;
-            int numberOfCompletedRow;
-            int numberOfRemainingPicture;
+            int numberOfCompletedRows;
+            int numberOfRemainingPictures;
 
-            numberOfCompletedRow = numberOfPicture / numberOfPicturesInOneRow;
-            numberOfRemainingPicture = numberOfPicture % numberOfPicturesInOneRow;
-            Console.WriteLine("                       Специальная зона)" );
-            Console.WriteLine(" (Ricardo Milos.jpg) (Billy Harington.jpg) (Van Darkholme.jpg)" );
-            Console.WriteLine("                Картинок отображено - 3 из 3" );
-            Console.WriteLine("                         Ряд 1 из " + numberOfCompletedRow );
-            Console.WriteLine("                Картинок сверх меры - " + numberOfRemainingPicture );
+            numberOfCompletedRows = numberOfPictures / numberOfPicturesInOneRow;
+            numberOfRemainingPictures = numberOfPictures % numberOfPicturesInOneRow;
+            Console.WriteLine("                       Специальная зона)");
+            Console.WriteLine(" (Ricardo Milos.jpg) (Billy Harington.jpg) (Van Darkholme.jpg)");
+            Console.WriteLine("                Картинок отображено - 3 из " + numberOfPicturesInOneRow);
+            Console.WriteLine("                         Ряд 1 из " + numberOfCompletedRows);
+            Console.WriteLine("                Картинок сверх меры - " + numberOfRemainingPictures);
         }
     }
 }
