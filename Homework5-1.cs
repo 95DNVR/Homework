94304f59c0d9574dce4e031e06e58e33913025f8
95DNVR <95denver@gmail.com>
2023-06-05 20:42:07 +0300

    Create Homework5-1.cs

---
+++ Homework5-1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+
+            Console.WriteLine(" Число " + firstNumber + " больше числа " + secondNumber);
+            firstNumber++;
+            secondNumber--;
+            Console.WriteLine(" Число " + firstNumber + " больше числа " + secondNumber);
+        }
+    }
+}
