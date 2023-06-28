9358b128323df10cc808905cad4e1cc0197b14cb
95DNVR <95denver@gmail.com>
2023-06-28 22:38:49 +0300

    Create h.w.8.15.cs

---
+++ h.w.8.15.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h.w._8._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int triesCount;
+
+            Console.Write("Введите текст пожалуйста ");
+            userInput = Console.ReadLine();
+            Console.Write("Введите количество вывода текста ");
+            triesCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int finalValue = 1; finalValue <= triesCount; triesCount --)
+            {
+                Console.WriteLine(triesCount + ") " + userInput);
+            }
+        }
+    }
+}
