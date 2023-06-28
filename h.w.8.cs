3b72788a0caae50b8bd4d7b7413866278e88c8e9
95DNVR <95denver@gmail.com>
2023-06-28 18:40:53 +0300

    Create h.w.8.cs

---
+++ h.w.8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h.w._8._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int numberOfUserImput;
+
+            Console.Write("Введите текст пожалуйста ");
+            userInput = Console.ReadLine();
+            Console.Write("Введите количество вывода текста ");
+            numberOfUserImput = Convert.ToInt32(Console.ReadLine());
+
+            while (numberOfUserImput >= 1)
+            {
+                Console.WriteLine(numberOfUserImput + ") " + userInput);
+                numberOfUserImput--;
+            }
+        }
+    }
+}
