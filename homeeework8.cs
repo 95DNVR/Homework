b35c04c57e0aaa698bbd5c9e79d7a79eeb1aeb66
95DNVR <95denver@gmail.com>
2023-06-28 11:03:32 +0300

    Create homeeework8.cs

---
+++ homeeework8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userInput;
+
+            while (true)
+            {
+                Console.WriteLine("Введите код, пожалуйста: ");
+                userInput = Console.ReadLine();
+                if (userInput == stopWord)
+                {
+                    Console.WriteLine("Ну вот можешь же когда захочешь");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" Не,не,не давай по новой");
+                }
+            }
+        }
+    }
+}
