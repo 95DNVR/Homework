a609de6ef3c4c701415de0410a66271b854aad41
95DNVR <95denver@gmail.com>
2023-06-28 11:58:27 +0300

    Create hw 9.cs

---
+++ hw 9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userInput;
+
+            while (true)
+            {
+                Console.WriteLine("Введите ,пожалуйста, пароль ");
+                userInput = Console.ReadLine();
+                if (stopWord == userInput) 
+                {
+                    Console.WriteLine("- Вот пароль");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("- Это по твоему пароль?");
+                }
+            }
+        }
+    }
+}
