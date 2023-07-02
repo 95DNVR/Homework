122951b9c20d0f07a89e999489658e81e526957a
95DNVR <95denver@gmail.com>
2023-07-02 23:27:08 +0300

    Create homework 10.1.cs

---
+++ homework 10.1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int finalValue = 96;
+
+            for (int i = 5; i <= finalValue; i += 7)
+            {
+                Console.WriteLine(i);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
