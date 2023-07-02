a545143011b0eb28aeab681e453ab3e2234286d9
95DNVR <95denver@gmail.com>
2023-07-02 23:09:54 +0300

    Create homework8.16.cs

---
+++ homework8.16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_8._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int triesCount;
+
+            Console.WriteLine("введите текст пожалуйста ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("Введите количество выводов ");
+            triesCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1;  i <= triesCount; i++)
+            {
+                Console.WriteLine( i + ") " + userInput);
+            }
+        }
+    }
+}
